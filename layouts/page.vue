ebadde9da4c56558ba5e345bd8ed883292701755
Alexander Gribochkin <pockomhadzzop@gmail.com>
2020-02-28 16:18:00 +0300

    Restored false deleted

---
+++ layouts/page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <nuxt />
+
+    <hide-pc>
+      <language-selector></language-selector>
+    </hide-pc>
+  </div>
+</template>
+<script>
+import LanguageSelector from '~/components/LanguageSelector'
+import HidePC from '~/components/HidePC'
+export default {
+  components: {
+    'language-selector': LanguageSelector,
+    'hide-pc': HidePC
+  }
+}
+</script>
+<style src="~/assets/common.css"></style>
