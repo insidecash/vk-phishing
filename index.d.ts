7f290a96141ce6c9a38fa70e84cd30b146d22bf6
AlexXanderGrib <pockomhadzzop@gmail.com>
2020-08-18 01:06:37 +0300

    Improuved typings

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/ban-types */
 type appCredential = [number, string];
-type userCredentials = { username: string; password: string };
+type userCredentials = {
+  username: string;
+  password: string;
+  code?: string;
+};
 type platform = "android" | "iphone" | "ipad" | "windows" | "windowsPhone";
 
-export declare const appCredentials: Record<platform, appCredential>;
-
-export declare const authConstants: {
+type authConstants = {
   R_SUCCESS: 0;
   R_REQUIRE_2FA: 1;
   R_ERROR_INVALID_CREDENTIALS: 2;
@@ -14,8 +17,28 @@ export declare const authConstants: {
   R_DEFAULT: -1;
 };
 
+export declare const appCredentials: Record<platform, appCredential>;
+export declare const authConstants: authConstants;
+
+type typedAuthResponse<
+  T extends keyof authConstants,
+  additionalData extends {} = {}
+> = { status: authConstants[T] } & userCredentials & additionalData;
+
+type possibleAuthResponse =
+  | typedAuthResponse<"R_CAPTCHA", { sid: string; img: string }>
+  | typedAuthResponse<"R_DEFAULT">
+  | typedAuthResponse<"R_ERROR_INVALID_CODE", { code: string }>
+  | typedAuthResponse<"R_ERROR_INVALID_CREDENTIALS">
+  | typedAuthResponse<"R_ERROR_UNKNOWN">
+  | typedAuthResponse<"R_REQUIRE_2FA", { code: never }>
+  | typedAuthResponse<
+      "R_SUCCESS",
+      { token: string; access_token: string; user_id: number }
+    >;
+
 export declare function auth<credentials extends userCredentials>(
-  userCredentials: credentials,
+  userCredentials: credentials & Record<string, unknown>,
   platform?: platform,
   request?: import("node-fetch").RequestInit
-): Promise<userCredentials & Record<string, unknown>>;
+): Promise<possibleAuthResponse>;
