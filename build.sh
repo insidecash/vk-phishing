4bba0a6a5dc4e07a43ac348971045126736f521e
AlexXanderGrib <pockomhadzzop@gmail.com>
2020-08-19 19:11:22 +0300

    Update packager script

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ mkdir -p build
 cp -r ./static ./build
 cp -r ./plugins ./build
 cp -r ./__sapper__ ./build
-cp -r ./{config.yml,package.json,package-lock.json,Dockerfile,.dockerignore} ./build
+cp ./{config.yml,package.json,package-lock.json,yarn.lock,Dockerfile,.dockerignore} ./build
+cp ./{install,start}.{sh,bat} ./build
